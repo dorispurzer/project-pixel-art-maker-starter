cc70e71636ba47accdf764ac7c2847ee6ad44540
Doris <doris@purzer.de>
2021-02-05 22:07:32 +0100

    adjusted the designs.js to get the PAM alive

--- designs.js
+++ designs.js
@@ -1,10 +1,53 @@
+
+// submit button procedure to get the size
+document.getElementById('sizePicker').onsubmit = function() {
+  inputSubmission();
+};
+
+// size input function
+function inputSubmission() {
+  event.preventDefault();  // check again as vs studio code says event is depreciated,but still in MDN
+  let gridHeight = document.getElementById("inputHeight").value;
+  let gridWidth = document.getElementById("inputWidth").value;
+  makeGrid(gridHeight, gridWidth);
+}
+
+// once the size is submitted, call makeGrid() and generate the grid Table
+function makeGrid(gridHeight, gridWidth) {
+  let table = document.getElementById("pixelCanvas");
+  let gridTable = '';
+
+  for (let row = 1; row <= gridHeight; row +=1){
+    gridTable += "<tr class='row" + row + "'>";
+    for (let col = 1; col <= gridWidth; col +=1){
+      gridTable += "<td class='cell' id='row" + row + "col" + col + "'></td>";
+    }
+    gridTable += "</tr>";
+  }
+
+  // add generated grid Table into the html <table> element
+  table.innerHTML = gridTable;
+
+  // once the grid Table is displaying wait for click event
+  clickEvent();
+}
+
 // Select color input
-// Select size input
+let colorPicker = document.getElementById("colorPicker");
+let pickedColor = colorPicker.value;
 
-// When size is submitted by the user, call makeGrid()
+// once a color is selected, pick color value and return:
+colorPicker.addEventListener("input", function() {
+  pickedColor = colorPicker.value;
+  }, false);
 
-function makeGrid() {
-
-// Your code goes here!
-
+// listen to clicked events on color and cells
+function clickEvent() {
+  let clickedCell = document.getElementsByClassName('cell');
+  for (let i = 0; i < clickedCell.length; i +=1) {
+    clickedCell[i].addEventListener("click",  function(event) {
+      let clickedCell = event.target;
+      clickedCell.style.backgroundColor = pickedColor;
+    });
+  }
 }
